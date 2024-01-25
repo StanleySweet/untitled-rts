8a187af618180bfe0fff8288186eab1cb14e96cc
Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>
2024-01-26 00:10:48 +0100

    Add icons

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -7,20 +7,11 @@ signal interacted(body)
 @export var prompt_action : String = "interact"
 @export var enabled : bool = true
 
-func get_prompt():
+func get_identity() -> Identity:
 	if not self.enabled:
-		return "Button is disabled."
-	
-	var object : Identity = get_node("../../Identity")
-	print(object.generic_name)
-	print(IIdentity.IID_Identity)
-	print(IUnitAI.IID_UnitAI)
-	
-	var key_name = ""
-	for action : InputEvent in InputMap.action_get_events(prompt_action):
-		if action is InputEventKey:
-			key_name = OS.get_keycode_string(action.physical_keycode)
-	return prompt_message + "\n[" + key_name + "]"
+		return null
+
+	return get_node("../../Identity")
 
 func interact(body):
 	if self.enabled:
--- scripts/Interactor.gd
+++ scripts/Interactor.gd
@@ -1,25 +1,34 @@
 extends RayCast3D
 
-@onready var prompt : Label = $Prompt
+@onready var specific_name : Label = $SpecificName
+@onready var generic_name : Label = $GenericName
+@onready var icon : TextureRect = $Icon
 
 var last_interactable : Interactable = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#add_exception(owner)
-	prompt.text = ""
+	specific_name.text = ""
+	generic_name.text = ""
+	icon.texture = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float) -> void:
 	if self.last_interactable != null:
 		self.last_interactable = null
-		prompt.text = ""
+		specific_name.text = ""
+		generic_name.text = ""
+		icon.texture = null
 	
 	if is_colliding():
 		var detected = get_collider()
 		if detected is Interactable:
 			if self.last_interactable != detected:
-				prompt.text = detected.get_prompt()
+				var identity : Identity = detected.get_identity()
+				specific_name.text =identity.specific_name
+				generic_name.text = identity.generic_name
+				icon.texture = identity.icon
 				self.last_interactable = detected
 				
 			if Input.is_action_just_pressed(detected.prompt_action):
